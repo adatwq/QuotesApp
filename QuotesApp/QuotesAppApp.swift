265ce90cd113face9804edf358cdd182a6278e6a
Khawlah Khalid <khawlah1920@hotmail.com>
2023-11-30 11:39:15 +0300

    Initial Commit

---
+++ QuotesApp/QuotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesAppApp.swift
+//  QuotesApp
+//
+//  Created by Khawlah Khalid on 30/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct QuotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
